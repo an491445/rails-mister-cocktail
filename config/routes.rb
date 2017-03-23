402adf4d78f9b9f6e0c7b9601c3a3848f972d95d
Anu Maria Kokkarinen <anu-maria@hotmail.com>
2017-03-23 18:05:33 +0100

    Redirect root

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     resources :doses, only: [ :new, :create, :destroy ]
   end
 
+  root "cocktails#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
