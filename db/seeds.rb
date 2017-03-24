c1147ff0568a551f22cb790227801cf9274a267a
Anu Maria Kokkarinen <anu-maria@hotmail.com>
2017-03-24 13:44:05 +0100

    Add images

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,53 +10,5 @@ Dose.destroy_all
 Cocktail.destroy_all
 Ingredient.destroy_all
 
-cocktails = [
-  {
-    name: "Old Fashioned",
-    },
-  {
-    name: "Daiquiri",
-    },
-  {
-    name: "Margarita",
-    },
-  {
-    name: "Sidecar",
-    },
-  {
-    name: "French 75",
-    },
-  {
-    name: "Bloody Mary",
-    },
-  {
-    name: "Irish Coffee",
-    },
-  {
-    name: "Jack Rose",
-    },
-  {
-    name: "Negroni",
-    },
-  {
-    name: "Boulevardier",
-    },
-  {
-    name: "Sazerac",
-    },
-  {
-    name: "Vieux Carré",
-    },
-  {
-    name: "Ramos Gin Fizz",
-    },
-  {
-    name: "Mint Julep",
-    }
-]
-
 ingredients = %w(lemon ice mint leaves redbull jagermeister sugar tonic gin rhum)
 ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
-
-
-cocktails.each { |cocktail| Cocktail.create(cocktail) }
